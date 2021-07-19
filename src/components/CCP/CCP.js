5024757d5d9d3182313e020741ff564ac0e76549
ashrafali-v <ashruashrafali@gmail.com>
2021-07-19 19:46:47 +0530

    API integration testing

--- src/components/CCP/CCP.js
+++ src/components/CCP/CCP.js
@@ -1,116 +1,56 @@
 import { Component } from 'react';
-import React, { useState, useEffect, createRef, useRef } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import 'amazon-connect-streams';
 import AWS from 'aws-sdk';
 import logo from '../../assets/image-sources/logo.png';
-// import  './CCP.module.scss';
-
-// import styles from './CCP.module.scss';
-
-// class CCP extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.containerDiv = React.createRef();
-//   }
-//   componentDidMount() {
-//     var ccpURL = `https://schoolguard360.awsapps.com/connect/ccp-v2/`;
-//     ccpURL = `${ccpURL}softphone`;
-//     // eslint-disable-next-line no-undef
-//     connect.core.initCCP(this.containerDiv.current, {
-//       ccpUrl: ccpURL,
-//       loginPopup: false,
-//       softphone: {
-//         allowFramedSoftphone: true
-//       }
-//     });
-//     // eslint-disable-next-line no-undef
-//     connect.contact(function (contact) {
-//       contact.onConnecting(function (c) {
-//           var contactID = contact.contactId;
-//           const requestOptions = {
-//             method: 'GET',
-//             headers: {
-//               'Content-Type': 'application/json'
-//             }
-//           };
-//           fetch('https://d3l65qxge7dap6.cloudfront.net/connectkube/agent/realTimeTranscript/?contactId=90ead137-083d-4349-8703-a58f4864c5ff', requestOptions)
-//             .then(response => response.json())
-//             .then(data => {
-//               console.log(data);
-//           });
-
-//           alert(contactID);
-//       });
-//       contact.onConnected(function (c) {
-//           var contactID = contact.contactId;
-//       });
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="ccp">
-//         <div className="containerDiv" ref={this.containerDiv} />
-//         <div>
-//           right side bar
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default CCP;
-
 
 function CCP() {
-    //const containerDiv = React.createRef();
     const containerDiv = useRef(null);
     useEffect(() => {
         fetchItems();
     }, []);
     const [users, setUsers] = useState([]);
-    //const [loading, setLoading] = useState(true);
+    const [contactDetails, secontactDetails] = useState({});
+    const [loading, setLoading] = useState(true);
     const fetchItems = () => {
         var ccpURL = `https://schoolguard360.awsapps.com/connect/ccp-v2/`;
         ccpURL = `${ccpURL}softphone`;
         // eslint-disable-next-line no-undef
         connect.core.initCCP(containerDiv.current, {
             ccpUrl: ccpURL,
-            loginPopup: false,
+            loginPopup: true,
             softphone: {
-                allowFramedSoftphone: true
+                allowFramedSoftphone: true,
+                disableRingtone: false
             }
         });
         // eslint-disable-next-line no-undef
         connect.contact(function (contact) {
             contact.onConnecting(function (c) {
                 var contactID = contact.contactId;
-                alert(contactID);
             });
             contact.onConnected(function (c) {
                 var contactID = contact.contactId;
-                // const requestOptions = {
-                //   method: 'GET',
-                //   headers: {
-                //     'Content-Type': 'application/json'
-                //   }
-                // };
-                // fetch('http://54.159.204.234:8091/connectkube/agent/?contactId=09462ab4-3a8b-4df6-9fdc-152a636ae97b', requestOptions)
-                //   .then(response => response.json())
-                //   .then(data => {
-                //     console.log(data);
-                //   });
-                const users = [
-                    {
-                        "contactId": "90ead137-083d-4349-8703-a58f4864c5ff",
-                        "phoneNumber": "8607598008",
-                        "accountNumber": "6921991334",
-                        "authenticated": true
-                    }
-                ];
-                setUsers(users);
-                console.log(users);
-                alert(contactID);
+                const requestOptions = {
+                  method: 'GET',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                };
+                fetch('https://d3l65qxge7dap6.cloudfront.net/connectkube/agent/callInfo/?contactId=09462ab4-3a8b-4df6-9fdc-152a636ae97b', requestOptions)
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                    secontactDetails(data);
+                });
+                // const details ={
+                //   "contactId": "90ead137-083d-4349-8703-a58f4864c5ff",
+                //   "phoneNumber": "86075980081234",
+                //   "accountNumber": "6921991334",
+                //   "authenticated": true
+                // }
+                // secontactDetails(details);
+                // setLoading(false);
             });
         });
     }
@@ -118,52 +58,47 @@ function CCP() {
         <div class="master-container">
             <div class="row-child main-layout">
                 <div class="connect-widget-box">
-                    {/* <img src=""  alt="" class="logo"></img> */}
-                    
-                    {/* <img src={require('../../assets/image-sources/logo.png')} /> */}
-
                     <img src={logo} />
-
                     <div class="connect-widget">
                     <div className="containerDiv ccpcontainer" ref={containerDiv} ></div>
                     </div>
-
                 </div>
                 <div class="dashboard-box">
                     <h1><b>EIVR</b>Agent Dashboard</h1>
                     <div class="dashboard">
-                        <div class="user-details">
-                            <div class="details">
-                                <span class="icon icon-call-calling"></span>
-                                <label for="">Phone Number</label>
-                                <h3 class="phone-number">+1 409 234 986</h3>
+                        {
+                            <div class="user-details">
+                              <div class="details">
+                                  <span class="icon icon-call-calling"></span>
+                                  <label for="">Phone Number</label>
+                                  <h3 class="phone-number">{contactDetails.phoneNumber?contactDetails.phoneNumber:"--"}</h3>
+                              </div>
+                              <div class="details">
+                                  <span class="icon icon-personalcard"></span>
+                                  <label for="">Contact ID </label>
+                                  <h3 class="contact-id">{contactDetails.contactId?contactDetails.contactId:"--"}</h3>
+                              </div>
+                              <div class="details">
+                                  <span class="icon icon-key"></span>
+                                  <label for="">Authentication</label>
+                                  <h3 class="auth-id">{contactDetails.authenticated?"Yes":"--"}</h3>
+                              </div>
+                              <div class="details">
+                                  <span class="icon icon-home-hashtag"></span>
+                                  <label for="">Account Number</label>
+                                  <h3 class="account-number">{contactDetails.accountNumber?contactDetails.accountNumber:"--"}</h3>
+                              </div>
+                              <div class="system-helth">
+                                  <span class="icon icon-system-happy"></span>
+                                  <label for="">Sentiment Analysis</label>
+                                  <div class="health-progress">
+                                      <span class="active-happy"></span><span
+                                          class="active-happy"></span><span></span><span></span><span></span>
+                                      <h5 class="happy">Good</h5>
+                                  </div>
+                              </div>
                             </div>
-                            <div class="details">
-                                <span class="icon icon-personalcard"></span>
-                                <label for="">Contact ID </label>
-                                <h3 class="contact-id">000246583</h3>
-                            </div>
-                            <div class="details">
-                                <span class="icon icon-key"></span>
-                                <label for="">Authentication</label>
-                                <h3 class="auth-id">K456923458GHAN1245</h3>
-                            </div>
-                            <div class="details">
-                                <span class="icon icon-home-hashtag"></span>
-                                <label for="">Account Number</label>
-                                <h3 class="account-number">2364657892</h3>
-                            </div>
-                            <div class="system-helth">
-                                <span class="icon icon-system-happy"></span>
-                                <label for="">Sentiment Analysis</label>
-                                <div class="health-progress">
-                                    <span class="active-happy"></span><span
-                                        class="active-happy"></span><span></span><span></span><span></span>
-                                    <h5 class="happy">Good</h5>
-                                </div>
-                            </div>
-                        </div>
-
+                        }
                         <div class="accordian-box">
                             <div class="accordian">
                                 <div class="accordian-head">
